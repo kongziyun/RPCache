b4c0ebc71ae15d92c3c321fcd3af73bb55effeb9
Guanye Zheng <zguanye@beyster1695p27.engin.umich.edu>
2013-11-20 21:41:45 -0500

    change test.s to intel style, update compile_assembly

--- Gem5/test.s
+++ Gem5/test.s
@@ -1,12 +1,29 @@
 	.file	"test.c"
-	.section	.text.startup,"ax",@progbits
-	.p2align 4,,15
+	.intel_syntax noprefix
+	.text
 	.globl	main
 	.type	main, @function
 main:
 .LFB0:
 	.cfi_startproc
-	xorl	%eax, %eax
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register 6
+	mov	DWORD PTR [rbp-4], 0
+	mov	DWORD PTR [rbp-8], 0
+	jmp	.L2
+.L3:
+	mov	eax, DWORD PTR [rbp-8]
+	add	DWORD PTR [rbp-4], eax
+	add	DWORD PTR [rbp-8], 1
+.L2:
+	cmp	DWORD PTR [rbp-8], 9
+	jle	.L3
+	mov	eax, 0
+	pop	rbp
+	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
 .LFE0:
