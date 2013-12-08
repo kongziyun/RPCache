f720c95e6bc313c7c9a39a823cc31037256023b9
kongziyun <kongzy@umich.edu>
2013-12-08 03:03:55 -0500

    changed rdtsc function

--- AES_Attack/learning_Protected.c
+++ AES_Attack/learning_Protected.c
@@ -1,4 +1,5 @@
 #include <netinet/in.h>
+#include <stdint.h>
 #include <openssl/aes.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -11,6 +12,7 @@
 #include <poll.h>
 #include <string.h>
 #include <stdio.h>
+#include "rdtsc.h"
 #include <math.h>
 double packets;
 double ttotal;
@@ -22,6 +24,8 @@ double udev[16][256];
 char n[16];
 int s;
 int size;
+uint64_t lock = 1;
+uint64_t unlock = 0;
 unsigned char key[16];
 AES_KEY expanded;
 unsigned char zero[16];
@@ -34,15 +38,20 @@ unsigned int timestamp(void)
   asm volatile(".byte 15;.byte 49" : "=a"(bottom),"=d"(top)); return bottom;
 }
 */
+/*
 unsigned int timestamp(){
   unsigned a, d;
   asm volatile("rdtsc" : "=a"(a), "=d"(d));
   return ((unsigned long) a) | (((unsigned long)d)<<32);
 }
 
-void handle(char out[40],char in[],int len) {
+*/
+
+#define timestamp() rdtsc()
+
+void handle(char out[48],char in[],int len) {
   unsigned char workarea[len * 3]; int i;
-  for (i = 0;i < 40;++i) out[i] = 0; 
+  for (i = 0;i < 48;++i) out[i] = 0; 
 
   //*(unsigned int *) (out + 32) = timestamp();
 
@@ -51,13 +60,14 @@ void handle(char out[40],char in[],int len) {
 	//for (i = 0;i < 1;++i) 
 	out[i] = in[i];
   for (i = 16;i < len;++i) workarea[i] = in[i];
-  m5_setpid(1);
-  *(unsigned int *)(out + 32) = timestamp(); 
+  m5_setpid(lock);
+  *(unsigned long long *)(out + 32) = rdtsc(); 
 	AES_encrypt(in,workarea,&expanded);
   /* a real server would now check AES-based authenticator, */ /* process legitimate packets, and generate useful output */
   //for (i = 0;i < 1;++i) 
-  *(unsigned int *) (out + 36) = timestamp();
-  m5_setpid(-1);
+  *(unsigned long long *) (out + 40) = rdtsc();
+  m5_setpid(unlock);
+//  m5_unsetpid(1);
   i = 0;
 	out[16 + i] = scrambledzero[i];
   //*(unsigned int *)(out + 36) = timestamp();
@@ -68,7 +78,7 @@ struct sockaddr_in server;
 struct sockaddr_in client; socklen_t clientlen; int s;
 char in[1537];
 int r;
-char out[40];
+char out[46];
 
 void tally(double timing) {
   int j;
@@ -115,15 +125,16 @@ main(int argc,char **argv) {
   
   if (read(0,key,sizeof key) < sizeof key) return 111; 
   AES_set_encrypt_key(key,128,&expanded); 
-  m5_setpid(1);
+  m5_setpid(lock);
   AES_encrypt(zero,scrambledzero,&expanded);
-  m5_setpid(-1);
+  m5_setpid(unlock);
+  //m5_unsetpid(1);
   int power2=4;
   int j;
   //size = atoi(argv[1]);
   size = 16;
   char packet[2048];
-  char out[40];
+  char out[48];
   struct pollfd p;
   long long inputs = 0;
   
@@ -139,13 +150,13 @@ main(int argc,char **argv) {
       n[j] = packet[j];
       handle(out,packet,size);
       unsigned int timing;
-      timing = *(unsigned int *) (out + 36);
-      timing -= *(unsigned int *) (out + 32);
+      timing = *(unsigned long long *) (out + 40);
+      timing -= *(unsigned long long *) (out + 32);
       
-      if (timing < 10000){
+//      if (timing < 10000){
         //printf("tally\n");
         tally(timing);break;
-      }
+  //    }
     }
   
   ++inputs;
--- AES_Attack/testk.c
+++ AES_Attack/testk.c
@@ -1,5 +1,6 @@
 #include<math.h>
 #include <stdio.h>
+#include "rdtsc.h"
 unsigned int timestamp(){
   unsigned a, d;
   asm volatile("rdtsc" : "=a"(a), "=d"(d));
@@ -12,59 +13,53 @@ void main(void){
   char arrayA[64];
   char arrayB[64];
   int i,j;
-  unsigned int start, end;
-  arrayB[32]='d';
-  m5_setpid(0);
+  unsigned long long start, end;
+  //arrayB[32]='d';
+  printf("%ld\n", sizeof(unsigned long long));
+  m5_setpid(1);
 
   for (i = 0; i < 64; ++i)
     arrayA[i] = '1';
 
-//  m5_setpid(-1);
+  m5_setpid(0);
+  for (i = 0; i < 64; ++i)
+  {
+    m5_setpid(1);
+    start = rdtsc();
+    arrayA[i] = '1';
+    end = rdtsc();
+    m5_setpid(0);
+    printf("A: %d\ttime:%lld\n", i, end - start);
+
+    start = rdtsc();
+    arrayB[i] = '1';
+    end = rdtsc();
+    printf("B: %d\ttime:%lld\n\n", i, end - start);
+
+ }
 
   printf("sddd\n");
+   for (j = 0; j < 3; ++j)
   for (i = 0; i < 64;++i){
    
-      m5_setpid(0);
-      start = timestamp();
-      arrayA[i] = '3';
-      end = timestamp();
-      
-  //    m5_setpid(-1);
-      printf("%d\t A\ttime in between: %d\n", i, end - start);
-
-      start = timestamp();
+     start = rdtsc();
       arrayB[i] = '4';
-      end = timestamp();
-      printf("%d\t B\ttime in between: %d\n", i, end - start);
+      end = rdtsc();
+      printf("%d\t B\ttime in between: %lld\n", i, end - start);
+  }
 
-      printf("what \n");
-  } 
-
-
-
-
+  printf("sdfsdf\n\n");
+ m5_unsetpid(1);  
   for (j = 0; j < 3; ++j)
-  for (i = 0; i < 64; ++i){
-    m5_setpid(0);
-    start = timestamp();
-    arrayA[i] = '3';
-    end = timestamp();
-    printf("%d\t A\tseperate time: %d\n", i, end - start);
+  for (i = 0; i < 64;++i){
+   
+     start = rdtsc();
+      arrayB[i] = '4';
+      end = rdtsc();
+      printf("%d\t B\ttime in between: %lld\n", i, end - start);
   }
 
-//m5_setpid(-1);
-printf("\n");
 
-  for (i = 0; i < 64; ++i){
-    start = timestamp();
-    arrayB[i] = '3';
-    end = timestamp();
-    printf("%d\t B\tseperate time: %d\n", i, end - start);
-  }
 
-  //m5_unsetpid(0);
-  //m5_unsetpid(1);
-  printf("\n\n");
-  
-m5_setpid(1);
+ 
 }
